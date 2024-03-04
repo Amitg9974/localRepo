aea654658ce19bd56740e96ab2d3ad74e1f7b0aa
Amit Gupta <akg4071@gmail.com>
2024-03-04 10:53:30 +0530

    completed work on todo list

--- src/components/todo1.js
+++ src/components/todo1.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./todo1.css";
 
-function todo1() {
+function Todo1() {
+  const [data, setData] = useState("");
+  const [newData, setNewData] = useState([]);
+
+  const handleChange = (e) => {
+    setData(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    // e.preventDefault();
+    if (data.trim() !== "") {
+      setNewData([...newData, data]);
+      setData("");
+      // console.log(data);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleClick();
+    }
+  };
+
+  const handleClose = (e) => {
+    const deleteData = newData.filter((_, i) => i !== e);
+    setNewData(deleteData);
+  };
+
   return (
     <div>
-      todo
+      <div className="contain">
+        <input
+          className="input"
+          type="text"
+          name="todo"
+          value={data}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={handleClick} className="button">
+          Add
+        </button>
+      </div>
+      <div className="todo_list">
+        {newData.map((item, index) => (
+          <>
+            <p key={index} className="list">
+              {item}
+              <div className="close" onClick={() => handleClose(index)}>
+                X
+              </div>
+            </p>
+          </>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default todo1
+export default Todo1;
